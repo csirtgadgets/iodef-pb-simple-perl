7340abd833fac717d5762d48aba28bab05820b45
Wes Young <wes@barely3am.com>
2012-06-26 15:08:36 +0000

    bugfix

--- lib/Iodef/Pb/Simple/Plugin/Url.pm
+++ lib/Iodef/Pb/Simple/Plugin/Url.pm
@@ -7,6 +7,7 @@ use warnings;
 use URI::Escape;
 use Digest::SHA1 qw/sha1_hex/;
 use Digest::MD5 qw/md5_hex/;
+use Encode qw(encode_utf8);
 
 sub process {
     my $self = shift;
@@ -20,6 +21,8 @@ sub process {
     $addr = lc($addr);
     $addr =~ s/\/$//;
     my $safe = uri_escape($addr,'\x00-\x1f\x7f-\xff');
+    $safe = encode_utf8($addr);
+    warn $safe;
     $addr = $safe;
     $data->{'address'} = $safe;
     $data->{'md5'} = md5_hex($safe) unless($data->{'md5'});
