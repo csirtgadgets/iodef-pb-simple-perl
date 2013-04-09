a6b03b64a655a180c62814b6780d393e34677485
Wes Young <wes@barely3am.com>
2013-04-09 14:17:54 +0000

    timestamp fixes

--- lib/Iodef/Pb/Simple/Plugin/Detecttime.pm
+++ lib/Iodef/Pb/Simple/Plugin/Detecttime.pm
@@ -10,11 +10,10 @@ sub process {
     my $iodef = shift;
      
     my $dt = $data->{'detecttime'};
-    # default it to the hour
     unless($dt){
         require DateTime;
         $dt = DateTime->from_epoch(epoch => time());
-        $dt = $dt->ymd().'T'.$dt->hour().':00:00Z';
+        $dt = $dt->ymd().'T'.$dt->hms().'Z';
     }
     unless(ref($dt) eq 'DateTime'){
         $dt = $self->SUPER::normalize_timestamp($dt);
