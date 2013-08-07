cb977c2fda050a69fbf22f141b693dbcd98f7b35
Wes Young <wes@barely3am.com>
2013-08-07 12:12:26 +0000

    fixes https://github.com/collectiveintel/cif-v1/issues/46

--- lib/Iodef/Pb/Format/Snort.pm
+++ lib/Iodef/Pb/Format/Snort.pm
@@ -42,9 +42,11 @@ sub write_out {
         my $portlist = ($_->{'portlist'}) ? $_->{'portlist'} : 'any';       
 
         my $priority = 1;
-        for(lc($_->{'severity'})){
-            $priority = 5 if(/medium/);
-            $priority = 9 if(/high/);
+        if($_->{'severity'}){
+            for(lc($_->{'severity'})){
+                $priority = 5 if(/medium/);
+                $priority = 9 if(/high/);
+            }
         }
 
         my $dstnet      = 'any';
